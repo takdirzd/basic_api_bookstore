66e8ae2846ea309b6d359c5eb03e88fee2214cd8
Takdir Zulhaq D <88231577+takdirzd@users.noreply.github.com>
2025-04-23 20:51:26 +0700

    Add files via upload

---
+++ backend/utils/response.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"bookstore/model"
+)
+
+func JSONResponse(w http.ResponseWriter, status int, method, description string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	resp := model.Response{
+		Status:      status,
+		Method:      method,
+		Description: description,
+	}
+
+	json.NewEncoder(w).Encode(resp)
+}
