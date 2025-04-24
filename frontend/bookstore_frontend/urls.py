d8e7e0c5d18514c74a43d9d537069a1ec2758559
Takdir Zulhaq D <88231577+takdirzd@users.noreply.github.com>
2025-04-24 11:58:05 +0700

    Delete frontend directory

--- frontend/bookstore_frontend/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views  # import views dari folder yang sama
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index, name='index'),  # tambahkan ini
-]
--- frontend/bookstore_frontend/views.py
+++
@@ -1,5 +0,0 @@
-# bookstore_frontend/views.py
-from django.shortcuts import render
-
-def index(request):
-    return render(request, 'index.html')
